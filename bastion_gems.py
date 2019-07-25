954477ae73e403d59e028ff4573a4e8932a614fc
Shelll <46375898+Shellloman@users.noreply.github.com>
2019-07-25 11:56:44 +0200

    Update bastion_gems.py

--- bastion_gems.py
+++ bastion_gems.py
@@ -12,11 +12,12 @@ message_gamble = ["tu as remporté le pari ! tu obtiens ","Une grande victoire p
 # se sont les phrases prononcé par le bot pour plus de diversité
 # il n'y en a pas bcp donc je les laisse directement dans le code
 anti_spam = 6
+anti_spam2 = 3
 # nb de sec nécessaire entre 2 commandes
 
-TOKEN = "**************************************"
-#**************************************Bastion's Gems
-#
+TOKEN = "********************************"
+# ***********************************Bastion's Gems
+# ***********************************Bot_RPG
 #le token permet de reconnaitre mon bot
 
 client = discord.Client()
@@ -26,11 +27,12 @@ async def on_message(message):
 # on ne prend que la version miniscule du message
 # pour l'instant chaque commande que le bot detecte commence par if message.con...
 
+	data = sqlite3.connect('players.db')
+	c = data.cursor()
+	# on ouvre la base de donnée une fois au lancement du Bot
 	if message.content.startswith('ba.begin'):
 	#cette fonction initialise les données dans la bdd
 		ID = message.author.id
-		data = sqlite3.connect('players.db')
-		c = data.cursor()
 		c.execute("""SELECT ID FROM donnees WHERE ID=?""", (ID,))
 		rep = c.fetchone()
 		print (rep)
@@ -43,16 +45,12 @@ async def on_message(message):
 			# on injecte 3 données l'ID discord, le nombre de gems,et la date de
 			# la dernière commande, qui permet de vérifier de faire mon anti spam
 			c.execute("""INSERT INTO inventaire VALUES(?,?,?,?,?,?,?,?,?,?,?)""",(ID,0,0,0,0,0,0,0,0,0,0))
-
 			data.commit()
-			data.close()
 			msg = "fiche personnage créé !"
 		await client.send_message(message.channel, msg)
 
 	if message.content.startswith('ba.crime'):
 		ID = message.author.id
-		data = sqlite3.connect('players.db')
-		c = data.cursor()
 		c.execute("""SELECT time FROM donnees WHERE ID=?""", (ID,))
 		time = c.fetchone()
 		# on récupère le la date de la dernière commande
@@ -66,30 +64,25 @@ async def on_message(message):
 			msg = "il faut attendre "+str(anti_spam)+" secondes entre chaque commande !"
 		await client.send_message(message.channel, msg)
 		data.commit()
-		data.close()
 
 	if message.content.startswith('ba.bal'):
 		ID = message.author.id
-		data = sqlite3.connect('players.db')
-		c = data.cursor()
 		c.execute("""SELECT time FROM donnees WHERE ID=?""", (ID,))
 		time = c.fetchone()
-		if time[0] < t.time()-5:
+		if time[0] < t.time()-anti_spam2:
 			c.execute("""UPDATE donnees SET time = ? WHERE ID = ?""",(t.time(),ID))
 			c.execute("""SELECT gem FROM donnees WHERE ID=?""", (ID,))
 			gem = c.fetchone()
 			msg = "tu as actuellement : "+str(gem[0])+" :gem: !"
 		else:
-			msg = "il faut attendre 5 secondes entre chaque commande !"
+			msg = "il faut attendre "+str(anti_spam2)+" secondes entre chaque commande !"
 		await client.send_message(message.channel, msg)
 		data.commit()
-		data.close()
+
 
 	if message.content.startswith('ba.gamble'):
 		valeur = int(message.content[10:])
 		ID = message.author.id
-		data = sqlite3.connect('players.db')
-		c = data.cursor()
 		c.execute("""SELECT time FROM donnees WHERE ID=?""", (ID,))
 		time = c.fetchone()
 		if time[0] < t.time()-anti_spam:
@@ -106,22 +99,17 @@ async def on_message(message):
 			msg = "il faut attendre "+str(anti_spam)+" secondes entre chaque commande !"
 		await client.send_message(message.channel, msg)
 		data.commit()
-		data.close()
+
 
 	if message.content.startswith('ba.buy pickaxe'):
 		ID = message.author.id
-		data = sqlite3.connect('players.db')
-		c = data.cursor()
 		c.execute("""UPDATE donnees SET gem = gem - ? WHERE ID = ?""",(20,ID))
 		c.execute("""UPDATE inventaire SET pickaxe = pickaxe + ? WHERE ID = ?""",(1,ID))
 		data.commit()
-		data.close()
 		await client.send_message(message.channel, "tu as désormais une pioche en plus !")
 
 	if message.content.startswith('ba.mine'):
 		ID = message.author.id
-		data = sqlite3.connect('players.db')
-		c = data.cursor()
 		c.execute("""SELECT time FROM donnees WHERE ID=?""", (ID,))
 		time = c.fetchone()
 		if time[0] < t.time()-anti_spam:
@@ -144,7 +132,7 @@ async def on_message(message):
 			msg = "il faut attendre "+str(anti_spam)+" secondes entre chaque commande !"
 		await client.send_message(message.channel, msg)
 		data.commit()
-		data.close()
+
 
 	if message.content.startswith('ba.help'):
 		msg = "**Bienvenue dans l'aide de Bastion's gems !** \n Il y a actuellement 6 commandes :\n\
@@ -158,21 +146,19 @@ async def on_message(message):
 
 	if message.content.startswith('ba.inv'):
 		ID = message.author.id
-		data = sqlite3.connect('players.db')
-		c = data.cursor()
 		c.execute("""SELECT time FROM donnees WHERE ID=?""", (ID,))
 		time = c.fetchone()
-		if time[0] < t.time()-5:
+		if time[0] < t.time()-anti_spam2:
 			c.execute("""UPDATE donnees SET time = ? WHERE ID = ?""",(t.time(),ID))
 			c.execute("""SELECT pickaxe,cobblestone,iron,gold,diamond FROM inventaire WHERE ID=?""", (ID,))
 			inv = c.fetchone()
 			print (inv)
 			msg = "**ton inventaire**\n```-pickaxe.s : "+str(inv[0])+"\n-cobblestone.s : "+str(inv[1])+"\n-iron.s : "+str(inv[2])+"\n-gold: "+str(inv[3])+"\n-diamond : "+str(inv[4])+"```"
 		else:
-			msg = "il faut attendre 5 secondes entre chaque commande !"
+			msg = "il faut attendre "+str(anti_spam2)+" secondes entre chaque commande !"
 		await client.send_message(message.channel, msg)
 		data.commit()
-		data.close()
+
 
 	if message.content.startswith('ba.sell'):
 		i = 0
@@ -196,6 +182,13 @@ async def on_message(message):
 		gain = int(nb)*mult
 		msg = "tu veux vendre "+nb+" "+item+" pour "+str(gain)+":gem:"
 		await client.send_message(message.channel, msg)
+
+	if message.content.startswith('ba.rebdd') and message.author.id == 141883318915301376 :
+		data.commit()
+		data.close()
+		data = sqlite3.connect('players.db')
+		c = data.cursor()
+
 @client.event
 async def on_ready():
 	print('Logged in as')
@@ -205,4 +198,3 @@ async def on_ready():
 	print('------')
 
 client.run(TOKEN)
-
